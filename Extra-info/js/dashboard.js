a04ef80d6480aa1e5fec0378a31087d21ba31b48
Ahmad <ahmadtaqi789@gmail.com>
2022-05-15 17:48:35 -0400

    Dashboard improvements

--- Extra-info/js/dashboard.js
+++ Extra-info/js/dashboard.js
@@ -1,8 +1,8 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 //User info
-var access_token="eyJraWQiOiIrRUlCVHBVTngyeGhcLzg3MEpSUEh0T2QzRUdlRU1wZG1EZ3UxTkpTMWptOD0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiRnlIMkRwcTJzckQ0aV9TcXZCQThmQSIsInN1YiI6IjI2OGU4YjhjLTAxOWQtNGExNC1iZWUxLTA2Y2QwZDE5Y2NjMiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9MVDZmT1ZnQ2IiLCJjb2duaXRvOnVzZXJuYW1lIjoiYmMzMTc3IiwiYXVkIjoiNTU5YjF0Nm1scmtocnAyMHZvbjlxNHYyYWQiLCJldmVudF9pZCI6IjZiZTU4NTA1LTVkNjEtNDA3Yy04YTM2LTVhM2ZkYTkxYThhOSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjUyNTg5NjcyLCJleHAiOjE2NTI1OTMyNzIsImlhdCI6MTY1MjU4OTY3MiwianRpIjoiNDQwZTMwNTEtMDZhNy00OWRjLTg3NjAtMjdmMTYzNjJhYzM2IiwiZW1haWwiOiJiYzMxNzdAbnl1LmVkdSJ9.BLWfDldpTAAfEDyYd3eiAcFXGLbbmoDoswPCdwMPm2xG8VuU9tj9Y0QIAwK7Lfffs1tldVpKZCjXRbLWyrRzz3-L0QBN5oIW396pJKDE16KwwooDp_uI7JBLscyXs0Aghh4XxjCMtJMHdf0s_CgTj1dStl7n0K-0-FfdDkFdT4YJnuEOlSCqdyEjdaOhX_eYibC-p3fEbPqhbvQtZbojVk9IbKx9tpt-Nm76sJNyE1Jzc9XKmZJ1sf_UGXTJ3vzS-qIV9qu2YdGL2RLpy1N_6neXacxUbisCbPpt3szNJoWu3m4_X7KLfPOQuafWM9o7zw3rqADqd_e3wHreB5Xb6g";
-//localStorage.getItem("id_token");
+var access_token="eyJraWQiOiIrRUlCVHBVTngyeGhcLzg3MEpSUEh0T2QzRUdlRU1wZG1EZ3UxTkpTMWptOD0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiNDdNUEpSbmVxVEZQNnBiM2t0X3dDdyIsInN1YiI6IjI2OGU4YjhjLTAxOWQtNGExNC1iZWUxLTA2Y2QwZDE5Y2NjMiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9MVDZmT1ZnQ2IiLCJjb2duaXRvOnVzZXJuYW1lIjoiYmMzMTc3IiwiYXVkIjoiNTU5YjF0Nm1scmtocnAyMHZvbjlxNHYyYWQiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY1MjY0NzczMCwiZXhwIjoxNjUyNjUxMzMwLCJpYXQiOjE2NTI2NDc3MzAsImp0aSI6ImJlNmUzOTMwLTdjNDEtNGRkYi04NzVhLTRiNGIzN2M2OGI5MSIsImVtYWlsIjoiYmMzMTc3QG55dS5lZHUifQ.JGKX2uRn1uGGDtRH94wsIn1JeJ2sLpJiB5cp4X2HEB7gTp6Uuv3U7nj6kGAkg06QNqkhkVMDAaDvTt78SHMg5Z5rvcPC78q-8dcixZVv_cn8BdBmI6uGNPh3LcCIDiM1AahPTc5BxHJTzLo31GxGuvDIoE383_deCPYlzzQfpNxq7CJojnRL7ac8Y3xqx--zov1YBFcmVbKAXwSzNipHRvLcLwKAZqrvx-yX5h4Bl8MHnFkFGaJZXj30b5vXUa7Ar9RvIQHFFa3JbAdD8LXmiwtiP7EgbLVV9kVqfd8u7om085i0SLoOXY0kaBkTfvS2gW2ekRFEp2mnGxyjymPhTA";
+//var access_token = localStorage.getItem("id_token");
 console.log(access_token);
 var params={
     "Authorization":access_token
@@ -14,7 +14,9 @@ var apigClient = apigClientFactory.newClient({ });
 apigClient.dashboardTopwishlistedGet(params, body , {}).then(function(res){
   console.log(res["data"]);
   res["data"].forEach(function(subject) {
-    document.getElementById('wishlist-items').innerHTML += '<li class="list-group-item">' + subject.name + '</li>'  
+    // <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+    document.getElementById('wishlist-items').innerHTML += '<a href ="'+ 'review.html?q=' + subject.courseid + '" class="list-group-item list-group-item-action">' + subject.name + '</a>' 
+    //document.getElementById('wishlist-items').innerHTML += '<li class="list-group-item">' + subject.name + '</li>'  
   });
     
 }).catch(function(result){
@@ -24,7 +26,8 @@ apigClient.dashboardTopwishlistedGet(params, body , {}).then(function(res){
 apigClient.dashboardTopratedGet(params, body , {}).then(function(res){
   console.log(res["data"]);
   res["data"].forEach(function(subject) {
-    document.getElementById('rated-items').innerHTML += '<li class="list-group-item">' + subject.name + '</li>'  
+    document.getElementById('rated-items').innerHTML += '<a href ="'+ 'review.html?q=' + subject.id + '" class="list-group-item list-group-item-action">' + subject.name + " | Avg Rating: "+ parseFloat(subject.rating).toFixed(2) + '</a>'    
+    //document.getElementById('rated-items').innerHTML += '<li class="list-group-item">' + subject.name + '</li>'  
   });  
 }).catch(function(result){
     console.log("Error in fetching top rated items!");
@@ -33,7 +36,8 @@ apigClient.dashboardTopratedGet(params, body , {}).then(function(res){
 apigClient.dashboardMostreviewedGet(params, body , {}).then(function(res){
   console.log(res["data"]);
     res["data"].forEach(function(subject) {
-    document.getElementById('reviewed-items').innerHTML += '<li class="list-group-item">' + subject.name + '</li>'  
+    document.getElementById('reviewed-items').innerHTML += '<a href ="'+ 'review.html?q=' + subject.id + '" class="list-group-item list-group-item-action">' + subject.name + '</a>'    
+    //document.getElementById('reviewed-items').innerHTML += '<li class="list-group-item">' + subject.name + '</li>'  
   });
 }).catch(function(result){
     console.log("Error in fetching top reviewed items!");
