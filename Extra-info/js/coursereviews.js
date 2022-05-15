8c5d686cf60261dfdcc71070036dae3550dc63b5
therealbappi <chi.bharathsai@gmail.com>
2022-05-15 11:19:58 -0400

    Homepage and course review

--- Extra-info/js/coursereviews.js
+++ Extra-info/js/coursereviews.js
@@ -3,7 +3,7 @@
 //console.log("in reviews");
 //console.log(access_token);
 //access_token="eyJraWQiOiIrRUlCVHBVTngyeGhcLzg3MEpSUEh0T2QzRUdlRU1wZG1EZ3UxTkpTMWptOD0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiNUN5UEI4Y0RKRmJ0UGNoNGJuNm5iQSIsInN1YiI6IjAwMzI1YWM2LTE5YWQtNGY0MC05MDFhLTBlNjc2ZTkwNmZhMSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9MVDZmT1ZnQ2IiLCJjb2duaXRvOnVzZXJuYW1lIjoiYmFwcGkiLCJhdWQiOiI1NTliMXQ2bWxya2hycDIwdm9uOXE0djJhZCIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjUyNTU1NTAzLCJleHAiOjE2NTI1NTkxMDMsImlhdCI6MTY1MjU1NTUwMywianRpIjoiMmZkYTlkZGMtZTI3Yy00N2RiLTg2M2MtZTcxZGMwM2Q4MzM1IiwiZW1haWwiOiJjaGkuYmhhcmF0aHNhaUBnbWFpbC5jb20ifQ.IDZWeKhWjVwL2Z0wcm1zsP36ApELMQ0vFtAcnJ9wG-6dHOdkvvWPwYddsV-W9ZgMSV76HZrxHn5I9Z3Dx0RICl8k1llF6-IaXyd4Yc3Qj8o7xFZtDg3CYLn-67C9bYg_EE97pIc3rtEJyvS2UOkJxNVpKceAXs-cWaXSWeVY7PaswmVy69yF232gcEdeDORR8NrIzecjhtjP6eBk5lAO_-8Rq8aDyVoJccy7eVgRMSX1mvplXP1yk-294FvzB0Ai2ahrqXaMRKKfjQ382WrBIk3UxWTnRpGSd1ZQxYvatrsMdC02tipPYY_BWfJLCXK9Savjlsl790fXy-5dyMmVPw";
-var access_token="eyJraWQiOiIrRUlCVHBVTngyeGhcLzg3MEpSUEh0T2QzRUdlRU1wZG1EZ3UxTkpTMWptOD0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiRnlIMkRwcTJzckQ0aV9TcXZCQThmQSIsInN1YiI6IjI2OGU4YjhjLTAxOWQtNGExNC1iZWUxLTA2Y2QwZDE5Y2NjMiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9MVDZmT1ZnQ2IiLCJjb2duaXRvOnVzZXJuYW1lIjoiYmMzMTc3IiwiYXVkIjoiNTU5YjF0Nm1scmtocnAyMHZvbjlxNHYyYWQiLCJldmVudF9pZCI6IjZiZTU4NTA1LTVkNjEtNDA3Yy04YTM2LTVhM2ZkYTkxYThhOSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjUyNTg5NjcyLCJleHAiOjE2NTI1OTMyNzIsImlhdCI6MTY1MjU4OTY3MiwianRpIjoiNDQwZTMwNTEtMDZhNy00OWRjLTg3NjAtMjdmMTYzNjJhYzM2IiwiZW1haWwiOiJiYzMxNzdAbnl1LmVkdSJ9.BLWfDldpTAAfEDyYd3eiAcFXGLbbmoDoswPCdwMPm2xG8VuU9tj9Y0QIAwK7Lfffs1tldVpKZCjXRbLWyrRzz3-L0QBN5oIW396pJKDE16KwwooDp_uI7JBLscyXs0Aghh4XxjCMtJMHdf0s_CgTj1dStl7n0K-0-FfdDkFdT4YJnuEOlSCqdyEjdaOhX_eYibC-p3fEbPqhbvQtZbojVk9IbKx9tpt-Nm76sJNyE1Jzc9XKmZJ1sf_UGXTJ3vzS-qIV9qu2YdGL2RLpy1N_6neXacxUbisCbPpt3szNJoWu3m4_X7KLfPOQuafWM9o7zw3rqADqd_e3wHreB5Xb6g";
+var access_token="eyJraWQiOiIrRUlCVHBVTngyeGhcLzg3MEpSUEh0T2QzRUdlRU1wZG1EZ3UxTkpTMWptOD0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiem1kNVpWd2J3Zkk0RUx1Z3VuM09KdyIsInN1YiI6IjI2OGU4YjhjLTAxOWQtNGExNC1iZWUxLTA2Y2QwZDE5Y2NjMiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9MVDZmT1ZnQ2IiLCJjb2duaXRvOnVzZXJuYW1lIjoiYmMzMTc3IiwiYXVkIjoiNTU5YjF0Nm1scmtocnAyMHZvbjlxNHYyYWQiLCJldmVudF9pZCI6IjQ4M2VlOTM5LTdiYzEtNGM2YS05YjBhLTQzODBlMTI3MjJmZSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjUyNjI0MzA4LCJleHAiOjE2NTI2Mjc5MDgsImlhdCI6MTY1MjYyNDMwOCwianRpIjoiM2YzZDcyMGQtZjU1ZS00NjM1LWE4ZTYtYmM4ZGQ0YzRhMzRmIiwiZW1haWwiOiJiYzMxNzdAbnl1LmVkdSJ9.Q_jFH9oftujoo-beL5Hm3rAs2cxZUSqQbPWkyZJeuYaXyvIxtc4qTSY2YOk3Isa7Cta6cjOH1PbKHQobNa2GsFIpX43_fiFlXssohTmSOIcRGx9uC6dkSHguDUNgg7Mjvf1uCIYXJ5ju4UEid0qYBsYP0b_YBOy7YKS9kQ-v0ARL4go1YAxl-gp5XBR8maxh9ysmJIbPtkhxHdPxi9dTD8SmkpRN4JAKBi6FPlJ_ikvSLo0aVirHn36ueXbcHvo6NTEXcffrLo8MzdQd8o0kOVLVVXaQbQLSN_4_07EXbLLoYqWR37_iIA3_QXgCGwL_XcVt7daC8DhAx18hsME6GQ";
 var params={
     "Authorization":access_token,
     "school":"UC"    
@@ -21,10 +21,10 @@ apigClient.usersUserGet(params, body , {}).then(function(res){
   console.log(res["data"]);
   userdata=res["data"];
   
-  document.getElementById("username").innerHTML=document.getElementById("username").innerHTML+userdata["username"];
-  document.getElementById("school").innerHTML=document.getElementById("school").innerHTML+userdata["school"];
-  document.getElementById("program").innerHTML=document.getElementById("program").innerHTML+userdata["program"];
-  document.getElementById("semester").innerHTML=document.getElementById("semester").innerHTML+userdata["semester"];
+  document.getElementById("username").innerHTML=userdata["username"];
+  document.getElementById("school").innerHTML=userdata["school"];
+  document.getElementById("program").innerHTML=userdata["program"] + " Major";
+  document.getElementById("semester").innerHTML=userdata["semester"];
 
 
   
@@ -39,6 +39,8 @@ apigClient.schoolsGet(params, {} , {}).then(function(res){
 var schools = Object.keys(res['data']);
 var codes=Object.values(res['data'])
 var select = document.createElement("select");
+//class class="form-select"
+//select.className = "list-group-item";
 select.name = "schools";
 select.id = "schoollist"
 for (const val of schools)
@@ -77,7 +79,10 @@ let element = document.getElementById("drppr");
 while (element.firstChild) {
 element.removeChild(element.firstChild);
 }
-
+let element2 = document.getElementById("insidecouse");
+while (element2.firstChild) {
+element2.removeChild(element2.firstChild);
+}
 
 apigClient.schoolsProgramsGet(params, {} , {}).then(function(res){
   console.log(res['data']);
@@ -104,6 +109,10 @@ console.log(values);
 function funcprogram(){
     document.getElementById('schoolz').innerHTML=document.getElementById("schoollist").value;  
     document.getElementById('programz').innerHTML=document.getElementById("programlist").value; 
+    
+
+
+
 }
 
 //schools end
@@ -147,10 +156,13 @@ apigClient.schoolsProgramsGet(params, {} , {}).then(function(res){
       console.log("NO RESULT");
   });
 
-
+  let coursedata={}
 
   function searchcourse(){
-
+    let element3 = document.getElementById("insidecouse");
+    while (element3.firstChild) {
+    element3.removeChild(element3.firstChild);
+    }
     params={
     "Authorization":access_token,
     "school":    document.getElementById("schoolz").innerHTML.slice(-2),
@@ -162,11 +174,29 @@ apigClient.schoolsProgramsGet(params, {} , {}).then(function(res){
     apigClient.coursesGet(params, {} , {}).then(function(res){
   
   console.log(res['data']);
-  document.getElementById('courses').innerHTML=JSON.stringify(res['data']);
+  //document.getElementById('courses').innerHTML=JSON.stringify(res['data']);
+  coursedata=res['data'];
+
+  //append data to table
   
+  //console.log(coursedata);
+  for(const course in coursedata){
+    var li = document.createElement("li");
+    //console.log(coursedata[course]);
+    li.className = "list-group-item";
+    li.innerHTML=coursedata[course]['name'];
+    document.getElementById("insidecouse").appendChild(li);
+  }
+
+  //end
+
+
   }).catch(function(result){
       
       console.log("NO RESULT");
   });
+
+  
+  
 
   }
--- Extra-info/js/homepage.js
+++ Extra-info/js/homepage.js
@@ -1,7 +1,7 @@
 //User info
 //var access_token = localStorage.getItem("id_token");
 //console.log(access_token);
-var access_token="eyJraWQiOiIrRUlCVHBVTngyeGhcLzg3MEpSUEh0T2QzRUdlRU1wZG1EZ3UxTkpTMWptOD0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiRnlIMkRwcTJzckQ0aV9TcXZCQThmQSIsInN1YiI6IjI2OGU4YjhjLTAxOWQtNGExNC1iZWUxLTA2Y2QwZDE5Y2NjMiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9MVDZmT1ZnQ2IiLCJjb2duaXRvOnVzZXJuYW1lIjoiYmMzMTc3IiwiYXVkIjoiNTU5YjF0Nm1scmtocnAyMHZvbjlxNHYyYWQiLCJldmVudF9pZCI6IjZiZTU4NTA1LTVkNjEtNDA3Yy04YTM2LTVhM2ZkYTkxYThhOSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjUyNTg5NjcyLCJleHAiOjE2NTI1OTMyNzIsImlhdCI6MTY1MjU4OTY3MiwianRpIjoiNDQwZTMwNTEtMDZhNy00OWRjLTg3NjAtMjdmMTYzNjJhYzM2IiwiZW1haWwiOiJiYzMxNzdAbnl1LmVkdSJ9.BLWfDldpTAAfEDyYd3eiAcFXGLbbmoDoswPCdwMPm2xG8VuU9tj9Y0QIAwK7Lfffs1tldVpKZCjXRbLWyrRzz3-L0QBN5oIW396pJKDE16KwwooDp_uI7JBLscyXs0Aghh4XxjCMtJMHdf0s_CgTj1dStl7n0K-0-FfdDkFdT4YJnuEOlSCqdyEjdaOhX_eYibC-p3fEbPqhbvQtZbojVk9IbKx9tpt-Nm76sJNyE1Jzc9XKmZJ1sf_UGXTJ3vzS-qIV9qu2YdGL2RLpy1N_6neXacxUbisCbPpt3szNJoWu3m4_X7KLfPOQuafWM9o7zw3rqADqd_e3wHreB5Xb6g";
+var access_token="eyJraWQiOiIrRUlCVHBVTngyeGhcLzg3MEpSUEh0T2QzRUdlRU1wZG1EZ3UxTkpTMWptOD0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoiNjlDa0RDc0hfQlJWaFczVlJGMGNfUSIsInN1YiI6IjI2OGU4YjhjLTAxOWQtNGExNC1iZWUxLTA2Y2QwZDE5Y2NjMiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9MVDZmT1ZnQ2IiLCJjb2duaXRvOnVzZXJuYW1lIjoiYmMzMTc3IiwiYXVkIjoiNTU5YjF0Nm1scmtocnAyMHZvbjlxNHYyYWQiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY1MjYyNTg5MiwiZXhwIjoxNjUyNjI5NDkyLCJpYXQiOjE2NTI2MjU4OTIsImp0aSI6IjY3ZDYyNzYxLWQ1YWYtNGM4MC1hZDdjLWQ4OWE3Mzk5YjM3NyIsImVtYWlsIjoiYmMzMTc3QG55dS5lZHUifQ.sHrKzfNbMM6SB_VusZfiNvg-vC6eO3f6TKXwKObQjz5ABKruSleS3t2G9fUUrBdsSRkFsnI5Gw-HZ5024D1PKgFxa02cN75vpYq7UT-FYgqGisjkAIgR1MbyvaA15BjKmge-3X8dWzwygDj-xUsiUzPlEjVb7bKIwMBlAbZIz8kbKGG8rAwjVTX3ggpcUBK-HPZtLy31bhWP7g9Slm070c-NVuVlVpiAeEEbq1zzgO8wfSBGI7nIx3qtKCdl7bWvxbmfklUTZHEpfP-ZZpQ5Xuuo88WB15xAg_Yt53pIavlY0fteNovXy2ZEH1pLS9ao9DwBBi7JssZhkifkyCGZHA";
 
 var params={
     "Authorization":access_token
@@ -17,10 +17,11 @@ apigClient.usersUserGet(params, body , {}).then(function(res){
   console.log(res["data"]);
   userdata=res["data"];
   
-  document.getElementById("username").innerHTML=document.getElementById("username").innerHTML+userdata["username"];
-  document.getElementById("school").innerHTML=document.getElementById("school").innerHTML+userdata["school"];
-  document.getElementById("program").innerHTML=document.getElementById("program").innerHTML+userdata["program"];
-  document.getElementById("semester").innerHTML=document.getElementById("semester").innerHTML+userdata["semester"];
+  document.getElementById("username").innerHTML=userdata["username"];
+  document.getElementById("school").innerHTML=userdata["school"];
+  document.getElementById("program").innerHTML=userdata["program"] + " Major";
+  document.getElementById("semester").innerHTML=userdata["semester"];
+
 
 
   
@@ -34,9 +35,17 @@ apigClient.usersUserGet(params, body , {}).then(function(res){
 //function whishlistGet()
 apigClient.whishlistGet(params, body , {}).then(function(res){
   
-  console.log(res["data"][0]);
+  console.log(res["data"]);
+  var coursedata=res["data"];
   document.getElementById("whishlist").innerHTML=document.getElementById("whishlist").innerHTML+JSON.stringify(res["data"]);
-
+  for(const course in coursedata){
+    var li = document.createElement("li");
+    //console.log(coursedata[course]);
+    li.className = "list-group-item";
+    li.innerHTML=coursedata[course]['name'];
+    document.getElementById("whishlists").appendChild(li);
+  }
+ 
 
   
 }).catch(function(result){
