7f1f88334b358b96bff2f735021174bb1f6bc3b2
Viha Gupta <vihagupta95@gmail.com>
2022-05-16 01:12:20 -0400

    added sections in coursedetail including go to review btn. wishlist remaining

--- Extra-info/js/coursedetail.js
+++ Extra-info/js/coursedetail.js
@@ -4,7 +4,12 @@
 var access_token = "eyJraWQiOiIrRUlCVHBVTngyeGhcLzg3MEpSUEh0T2QzRUdlRU1wZG1EZ3UxTkpTMWptOD0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoicDdqYnVfeDhaa3pNLXNFeGdHa2sxdyIsInN1YiI6ImNiODlhOWY5LTAxYjctNDMxZC05MGFiLWMwZWFkMmU4MGMxZCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9MVDZmT1ZnQ2IiLCJjb2duaXRvOnVzZXJuYW1lIjoidmcyMjM3IiwiYXVkIjoiNTU5YjF0Nm1scmtocnAyMHZvbjlxNHYyYWQiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY1MjY3NTk5NiwiZXhwIjoxNjUyNzYyMzk2LCJpYXQiOjE2NTI2NzU5OTYsImp0aSI6IjRkMGQyMmFjLWIyZDUtNDU0NC04NGE5LWFhYTA0OWEwODJmMiIsImVtYWlsIjoidmcyMjM3QG55dS5lZHUifQ.tx8DV7LAVqIWNuH18s90sXYohp2C6B83NUU0nSU06EbABZ6tXp-9Py8nSecIJfRLywl7dtrcrsZyvl9sx4TBn0JMp858GWNJ6rTrRk_ixiJecMSLFvLtlADgTxFjiqyolVoJMB3WxafV20nZRGQ4IbFssGmCB1c5-E72G8pZK9Wz951TuvQjl5eKkT3S6dFRbNjjPCkuBAZ_Kdnf5W8GVPMXoGZznoO8JTLfIiTdg0NQ0kF9YHj87aHYf8FuO-KDm9CdUx48Z0gTuWNyya2Q7ixgDDf1VhbL4tH667L_wNtWkdgjZwvBWZlKBsdTVBnrAlvgKxy9KBZA46tCLKIqmw";
 var queryString_url = window.location.search;
 const urlParams = new URLSearchParams(queryString_url);
-const courseid = urlParams.get('q')
+const courseid = urlParams.get('courseid');
+const school = urlParams.get('school');
+const subject = urlParams.get('subject');
+const year = urlParams.get('year');
+const name = urlParams.get('name');
+const semester = urlParams.get('semester');
 function addreview(){
   //var btn=document.getElementById("button_for_new_review");
   var url="newreview.html?q="+courseid;
@@ -20,12 +25,75 @@ var body={
     }
 
 
+fetch(`https://schedge.a1liu.com/${year}/${semester}/search?full=true&school=${school}&subject=${subject}&query=${name}`).then((res)=>{
+  // fetch("https://schedge.a1liu.com/2021/fa/search?full=true&school=GY&subject=CS&query=Software Engineering I").then((res)=>{
+  res.json().then(obj=>{
+    console.log(obj);
+    const sections = obj[0]["sections"];
+    let detailsHTML = '';
+    for (let i = 0; i < sections.length; i++) {
+      detailsHTML = detailsHTML + `
+      <div class="card">
+        <div class="card-header" style="display: flex; justify-content: space-between">
+          <div style="max-width:70%">
+            <b>Name: </b>
+            <span style="max-width:75%">${sections[i]["name"]}</span>
+            <br>
+            <b>Code: </b>
+            ${sections[i]["code"]}
+            <b>Registration Number: </b>
+            ${sections[i]["registrationNumber"]}
+          </div>
+          <div>
+            <a href="review.html?q=${courseid}">
+              <button type="button" class="btn btn-primary">Go To Review</button>
+            </a>
+            <button type="button" class="btn btn-secondary" onclick="addToWishlist()">+ Wishlist</button>
+          </div>
+        </div>
+
+        <div class="card-body">
+          <b>Instructor: </b>
+          ${sections[i]["instructors"].join(', ')}
+          <br>
+          <b>Status: </b>
+          ${sections[i]["status"]}
+          <br>
+          <b>Type: </b>
+          ${sections[i]["type"]}
+          <b>Instruction Mode: </b>
+          ${sections[i]["instructionMode"]}
+          <br>
+          <b>Campus: </b>
+          ${sections[i]["campus"]}
+          <b>Location: </b>
+          ${sections[i]["location"]}
+          <br>
+          <b>Min Credits: </b>
+          ${sections[i]["minUnits"]}
+          <b>Max Credits: </b>
+          ${sections[i]["maxUnits"]}
+          <br>
+        </div>
+      </div><br>`
+  
+
+      
+      
+    }
+    document.getElementById("details").innerHTML=detailsHTML
+
+  })
+})
+
+
+
 //
 var apigClient = apigClientFactory.newClient({ });
 //Fetching course details
 apigClient.coursesCourseGet(params, body , {}).then(function(res){
   
-  console.log(res['data']);
+  // console.log(res['data']);
   //coursename
   document.getElementById("coursename").innerHTML=res['data']['name'];
   document.getElementById("recent_profs").innerHTML+=res['data']['recentprofessors'];
@@ -47,7 +115,7 @@ apigClient.coursesCourseGet(params, body , {}).then(function(res){
 apigClient.reviewsGet(params, body , {}).then(function(res){
   
   reviews=res['data'];
-  console.log(reviews);
+  // console.log(reviews);
   var total=0;
   var count=0;
   for(const review in reviews){
